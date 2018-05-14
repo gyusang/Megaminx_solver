c83b95aa782c0a16cb58e76ac0afe7acc7709eca
Sanggyu Lee <sanggyu523@naver.com>
2018-05-15 00:56:49 +0900

    Add colors in polygons

--- src/megaminx_gui/Inter.java
+++ src/megaminx_gui/Inter.java
@@ -4,6 +4,7 @@ public class Inter {
 	
 	public static final int SCREEN_WIDTH = 1280;
 	public static final int SCREEN_HEIGHT = 720;
+	public static final int RADIUS = 100;
 	
 	public static void main(String[] args) {
 		
--- src/megaminx_gui/Solver2.java
+++ src/megaminx_gui/Solver2.java
@@ -3,25 +3,22 @@ package megaminx_gui;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Polygon;
+import java.awt.Color;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Solver2 extends JFrame {
-	private int radius = 85;
-
 	private static final long serialVersionUID = -3119930909029998588L;
+	
+	int radius;
 	private Image ScreenImage;
 	private Graphics ScreenGraphic;
-
 	private Image Background;
-	// private Image Minxframe;
-	private Image[][] Minxpiece_up = new Image[13][10];
-	private Image[][] Minxpiece_down = new Image[13][10];
-	// private int xpoints[] = {5,100,100,5,5};
-	// private int ypoints[] = {5,5,100,100,5};
-	// private int npoints = 5;
-
+	
+	Color[] colors= new Color[13];
+	
+	@Override
 	public void paint(Graphics g) {
 		ScreenImage = createImage(Inter.SCREEN_WIDTH, Inter.SCREEN_HEIGHT);
 		ScreenGraphic = ScreenImage.getGraphics();
@@ -30,29 +27,48 @@ public class Solver2 extends JFrame {
 	}
 
 	public void screenDraw(Graphics g) {
-		g.drawImage(Background, 0, 0, null);
-		// g.drawImage(Minxframe, 0, 0, null);
-		// if(int [][] cube = 1 1 cube) {
-		// g.drawImage(Minxpiece_up[1][], 0, 0, null);
-		// };
-
-		int radius = 85;
-		int centerX = 500;
-		int centerY = 300;
-		double R = radius * Math.cos(Math.PI / 5);
-		Polygon frames[] = new Polygon[6];
-		for(int i=0;i<6;i++)
-			frames[i] = new Polygon();
-		for (int j = 0; j < 5; j++)
-			frames[5].addPoint((int) (centerX - radius * Math.sin(2 * j * Math.PI / 5)),
-					(int) (centerY - radius * Math.cos(2 * j * Math.PI / 5)));
-		for (int i = 0; i < 5; i++)
-			for(int j = 0;j < 5; j++)
-			frames[i].addPoint((int) (centerX - R * Math.sin(2 * i * Math.PI / 5)-radius*Math.sin(2*(i+j)*Math.PI/5)),
-					(int) (centerY + R * Math.cos(2 * i * Math.PI / 5)+radius*Math.cos(2*(i+j)*Math.PI/5)));
+		g.drawImage(Background, 0, 0, null);	
 		
-		for(int i=0;i<6;i++)
+		double R = 2 * radius * Math.cos(Math.PI / 5);
+		double r = radius*Math.cos(0);
+		int centerX = (int) Math.round(2 * R);
+		int centerY = (int) Math.round(2 * R);
+		Polygon frames[] = new Polygon[13];
+		for (int i = 1; i <= 12; i++)
+			frames[i] = new Polygon();
+
+		for (int j = 0; j < 5; j++)
+			frames[1].addPoint((int) (centerX - radius * Math.sin(2 * j * Math.PI / 5)),
+					(int) (centerY + radius * Math.cos(2 * j * Math.PI / 5)));
+
+		for (int i = 0; i < 5; i++)
+			for (int j = 0; j < 5; j++)
+				frames[6-i].addPoint(
+						(int) (centerX - R * Math.sin(2 * i * Math.PI / 5)
+								- radius * Math.sin(2 * (i + j) * Math.PI / 5)),
+						(int) (centerY - R * Math.cos(2 * i * Math.PI / 5)
+								- radius * Math.cos(2 * (i + j) * Math.PI / 5)));
+		centerX += 3 * R * Math.sin(3 * Math.PI / 5);
+		centerY += -R * Math.cos(2 * Math.PI / 5);
+		for (int j = 0; j < 5; j++)
+			frames[12].addPoint((int) (centerX - radius * Math.sin(2 * j * Math.PI / 5)),
+					(int) (centerY - radius * Math.cos(2 * j * Math.PI / 5)));
+
+		for (int i = 0; i < 5; i++)
+			for (int j = 0; j < 5; j++)
+				frames[7 + i].addPoint(
+						(int) (centerX - R * Math.sin(2 * i * Math.PI / 5)
+								- radius * Math.sin(2 * (i + j) * Math.PI / 5)),
+						(int) (centerY + R * Math.cos(2 * i * Math.PI / 5)
+								+ radius * Math.cos(2 * (i + j) * Math.PI / 5)));
+
+		for (int i = 1; i <= 12; i++) {
+//			g.setColor(colors[0]);
+//			g.drawPolygon(frames[i]);
+			g.setColor(colors[i]);
 			g.fillPolygon(frames[i]);
+		}
+		
 		// Polygon p = new Polygon();
 		// for (int i = 0; i < 5; i++)
 		// p.addPoint((int) (680 + 85 * Math.sin(i * 2 * Math.PI / 5)),
@@ -99,11 +115,24 @@ public class Solver2 extends JFrame {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
-
+		radius = Inter.RADIUS;
 		Background = new ImageIcon(Inter.class.getResource("../images/Background.png")).getImage();
-		// Minxframe = new
+		
 		// ImageIcon(Inter.class.getResource("../images/megaminxframe.png")).getImage();
-
+		colors[0] = new Color(0,0,0);
+		//White Purple DarkYellow DarkBlue Red DarkGreen LightGreen Orange LightBlue LightYellow Pink Gray
+		colors[1] = Color.white;
+		colors[2] = new Color(0,0,0); //TODO purple
+		colors[3] = Color.yellow;
+		colors[4] = Color.blue;
+		colors[5] = Color.red;
+		colors[6] = new Color(0,0,0);//TODO darkgreen
+		colors[7] = Color.green; 
+		colors[8] = Color.orange;
+		colors[9] = Color.cyan;
+		colors[10] = new Color(0,0,0); //TODO lightyellow
+		colors[11] = Color.pink;
+		colors[12] = Color.gray;
 	}
 
 }
